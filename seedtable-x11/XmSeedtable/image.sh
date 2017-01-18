b6967822c799956a821f072555d1878a5677e6ca
Narazaka <info@narazaka.net>
2017-01-18 20:33:32 +0900

    画像をいい感じに

--- seedtable-x11/XmSeedtable/image.sh
+++ seedtable-x11/XmSeedtable/image.sh
@@ -3,6 +3,6 @@ for K in `find ../../seedtable-gui/Resources -type f`;do
 	IN=$K
 	OUT=`echo $K | sed 's/.*\///g' | sed 's/\.png/.xpm/g'`
 	echo "$IN => $OUT"
-	convert -background white -flatten -alpha off $IN "`dirname $0`/Resources/$OUT"
+  convert $IN "(" +clone -alpha opaque -fill gray77 -colorize 100% ")" +swap -geometry +0+0 -compose Over -composite -alpha off "`dirname $0`/Resources/$OUT"
 done
 #convert ../../../seedtable-gui/Resources/excelToYaml.png Resources/excelToYaml.xpm
