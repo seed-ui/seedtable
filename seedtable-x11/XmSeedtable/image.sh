1a41dce4e753c382b51f22382d865b0e24a259fa
Narazaka <info@narazaka.net>
2017-01-18 17:14:23 +0900

    透過部分を白としてxpmをつくる

--- seedtable-x11/XmSeedtable/image.sh
+++ seedtable-x11/XmSeedtable/image.sh
@@ -3,6 +3,6 @@ for K in `find ../../seedtable-gui/Resources -type f`;do
 	IN=$K
 	OUT=`echo $K | sed 's/.*\///g' | sed 's/\.png/.xpm/g'`
 	echo "$IN => $OUT"
-	convert $IN "Resources/$OUT"
+	convert -background white -flatten -alpha off $IN "`dirname $0`/Resources/$OUT"
 done
 #convert ../../../seedtable-gui/Resources/excelToYaml.png Resources/excelToYaml.xpm
