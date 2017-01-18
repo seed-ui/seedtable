595e38d265613404049aee0d0793dd623170b12f
Narazaka <info@narazaka.net>
2017-01-18 21:07:00 +0900

    3.1

--- seedtable/AssemblyInfo.cs
+++ seedtable/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("seedtable")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("å¥êFìﬁó«ç„")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Narazaka")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("seedtable")]
+[assembly: AssemblyCopyright("MIT License")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("3.0.*")]
+[assembly: AssemblyVersion("3.1.*")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
