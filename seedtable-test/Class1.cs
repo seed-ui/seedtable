53b4f64b3cb6ff3a6c7625255f6d0d1c9d89a0f9
Narazaka <info@narazaka.net>
2017-05-19 01:48:18 +0900

    init test project

---
+++ seedtable-test/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace seedtable_test
+{
+    public class Class1
+    {
+        [Fact]
+        public void Test1() {
+            Assert.Equal(1, 1);
+        }
+    }
+}
