cd8c407c91bfd1a543bdde9fe753b519f6cc4545
Narazaka <info@narazaka.net>
2017-07-31 22:33:38 +0900

    doc

--- seedtable/DataDictionaryList.cs
+++ seedtable/DataDictionaryList.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 
 namespace SeedTable {
+    /** レコードDictionaryのリスト */
     public class DataDictionaryList {
+        /** [{id: 1, name: "foo"}, ...] */
         public IEnumerable<Dictionary<string, object>> Table { get; private set; }
 
+        /**
+         * コンストラクタ
+         *
+         * 渡されたレコードリストからidが空白であるものを省き、値が空白であるデータをnullに変換して受け取る
+         *
+         * @param table [{id: 1, name: "foo"}, ...]的なデータ
+         */
         public DataDictionaryList(IEnumerable<Dictionary<string, object>> table) {
             Table = table
                 .Where(rowData => rowData.ContainsKey("id") && rowData["id"] != null && (rowData["id"].ToString()).Length != 0)
                 .Select(rowData => rowData.ToDictionary(pair => pair.Key, pair => pair.Value is string && ((string) pair.Value) == "" ? null : pair.Value));
         }
 
+        /**
+         * {data1: {id: 1, name: "foo"}, ...}形式で返す
+         */
         public Dictionary<string, Dictionary<string, object>> ToDictionaryDictionary() {
             var dic = new Dictionary<string, Dictionary<string, object>>();
             foreach (var row in Table) {
@@ -19,6 +31,9 @@ namespace SeedTable {
             return dic;
         }
 
+        /**
+         * {"1": {id: 1, name: "foo"}, ...}形式で返す
+         */
         public Dictionary<string, Dictionary<string, object>> IndexById() {
             var dic = new Dictionary<string, Dictionary<string, object>>();
             foreach (var row in Table) {
@@ -27,6 +42,11 @@ namespace SeedTable {
             return dic;
         }
 
+        /**
+         * カットされたIDグループごとのレコードリスト
+         *
+         * {"data101": [{id: 10101, name: "foo"}], ...}形式で返す
+         */
         public Dictionary<string, List<Dictionary<string, object>>> ToSeparated(int preCut = 0, int postCut = 0) {
             var dic = new Dictionary<string, List<Dictionary<string, object>>>();
             foreach (var row in Table) {
@@ -40,6 +60,11 @@ namespace SeedTable {
             return dic;
         }
 
+        /**
+         * カットされたIDグループごとのレコードリスト
+         *
+         * {"data101": {data10101: {id: 10101, name: "foo"}}, ...}形式で返す
+         */
         public Dictionary<string, Dictionary<string, Dictionary<string, object>>> ToSeparatedDictionaryDictionary(int preCut = 0, int postCut = 0) {
             var dic = new Dictionary<string, Dictionary<string, Dictionary<string, object>>>();
             foreach (var row in Table) {
