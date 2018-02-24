c05fe33dd1013dc0be63a6cd865ea051058b9204
Narazaka <info@narazaka.net>
2018-02-24 20:37:40 +0900

    guiとx11で設定ファイルの互換性

--- seedtable-gui/FormValues.cs
+++ seedtable-gui/FormValues.cs
@@ -8,6 +8,8 @@ namespace seedtable_gui {
     public class FormValues {
         public string SeedPath { get; set; }
         public string SettingPath { get; set; }
+        public string SourcePath { get; set; } // FormValuesX11 で作られた設定との互換
+        public string YamlToExcelTargetFolder { get; set; } // FormValuesX11 で作られた設定との互換
 
         public FormValues() { }
         public FormValues(string seedPath, string settingPath) {
