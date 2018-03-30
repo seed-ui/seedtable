a5eaf47136ba0f5f7774e5ad2b2d08018ea0e275
Narazaka <info@narazaka.net>
2018-03-30 16:25:39 +0900

    3.26.0

--- seedtable-gui/Properties/AssemblyInfo.cs
+++ seedtable-gui/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.25.0")]
+[assembly: AssemblyVersion("3.26.0")]
