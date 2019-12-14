c6c1e28921e0dd2f033c1f977d2f6ca2a3001007
Narazaka <info@narazaka.net>
2019-12-14 19:16:20 +0900

    publish path

--- publish.bat
+++ publish.bat
@@ -1,6 +1,6 @@
-dotnet publish seedtable -c Release /p:PublishProfile=.seedtable/Properties/PublishProfiles/win-x86.pubxml
-dotnet publish seedtable -c Release /p:PublishProfile=.seedtable/Properties/PublishProfiles/linux-x64.pubxml
-dotnet publish seedtable -c Release /p:PublishProfile=.seedtable/Properties/PublishProfiles/osx-x64.pubxml
-dotnet publish seedtable -c Release /p:PublishProfile=.seedtable/Properties/PublishProfiles/portable.pubxml
-dotnet publish seedtable-gui -c Release /p:PublishProfile=.seedtable-gui/Properties/PublishProfiles/win-x86.pubxml
-dotnet publish seedtable-gui -c Release /p:PublishProfile=.seedtable-gui/Properties/PublishProfiles/portable.pubxml
+dotnet publish seedtable -c Release /p:PublishProfile=./seedtable/Properties/PublishProfiles/win-x86.pubxml
+dotnet publish seedtable -c Release /p:PublishProfile=./seedtable/Properties/PublishProfiles/linux-x64.pubxml
+dotnet publish seedtable -c Release /p:PublishProfile=./seedtable/Properties/PublishProfiles/osx-x64.pubxml
+dotnet publish seedtable -c Release /p:PublishProfile=./seedtable/Properties/PublishProfiles/portable.pubxml
+dotnet publish seedtable-gui -c Release /p:PublishProfile=./seedtable-gui/Properties/PublishProfiles/win-x86.pubxml
+dotnet publish seedtable-gui -c Release /p:PublishProfile=./seedtable-gui/Properties/PublishProfiles/portable.pubxml
