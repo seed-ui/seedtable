d2494423c8f7be718749559d4f44436ac83940a3
Narazaka <info@narazaka.net>
2019-12-14 20:12:42 +0900

    xm publish

---
+++ releases-xm-linux.sh
@@ -0,0 +1,2 @@
+mkdir -p releases
+zip -9 releases/XmSeedtable-linux.zip ./seedtable-x11/XmSeedtable/bin/Release/netcoreapp3.1/publish/linux-x64/XmSeedtable
---
+++ releases-xm-osx.sh
@@ -0,0 +1,2 @@
+mkdir -p releases
+zip -9 releases/XmSeedtable-osx.zip ./seedtable-x11/XmSeedtable/bin/Release/netcoreapp3.1/publish/osx-x64/XmSeedtable
