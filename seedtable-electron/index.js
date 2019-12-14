1fd8a5bc40189fbae2f578335f642268272cf6d3
junki.hiroi <junki.hiroi@drecom.co.jp>
2019-12-15 04:16:19 +0900

    formatter

--- seedtable-electron/index.js
+++ seedtable-electron/index.js
@@ -4,22 +4,21 @@ const BrowserWindow = electron.BrowserWindow;
 let mainWindow;
 
 app.on('window-all-closed', () => {
-    app.quit();
+  app.quit();
 });
 
 app.on('ready', () => {
-    // お使いの画面解像度に応じて調整してください
-    mainWindow = new BrowserWindow({
-        width: 600,
-        height: 400,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    });
+  mainWindow = new BrowserWindow({
+    width: 600,
+    height: 400,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
 
-    mainWindow.loadURL('file://' + __dirname + '/index.html');
-    // mainWindow.webContents.openDevTools(); // test
-    mainWindow.on('closed', ()  => {
-        mainWindow = null;
-    });
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  // mainWindow.webContents.openDevTools(); // test
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
 });
